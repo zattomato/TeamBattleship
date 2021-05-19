b154466c646e688b38b57d50f22ff149fb877882
Pitruder <haziqamirul744@gmail.com>
2021-05-19 13:10:58 +0800

    add project dashboard GUI and project creation form GUI

---
+++ IssueTracker/ProjectTable.java
@@ -0,0 +1,39 @@
+
+package IssueTracker;
+
+/**
+ * this class is use to represent the data fetched from external database
+ */
+public class ProjectTable {
+    private int projectID;
+    private String projectName;
+    private String description;
+    private int issueCount;
+
+    public ProjectTable(int projectID, String projectName, String description, int issueCount) {
+        this.projectID = projectID;
+        this.projectName = projectName;
+        this.description = description;
+        this.issueCount = issueCount;
+    }
+
+    public int getProjectID() {
+        return projectID;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getIssueCount() {
+        return issueCount;
+    }
+    
+    
+  
+    
+}
