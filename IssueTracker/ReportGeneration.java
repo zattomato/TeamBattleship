7d8fa0fd969075cc19e4ad976c78c145290ed140
User <User@azrulappy>
2021-06-13 19:48:24 +0800

    update comment page and issue dashboard, report generation.

--- IssueTracker/ReportGeneration.java
+++ IssueTracker/ReportGeneration.java
@@ -7,21 +7,16 @@ package IssueTracker;
 
 import ConnectionToDatabase.Cnx;
 import java.awt.Color;
-import java.sql.Statement;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import javax.swing.JOptionPane;
 import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartFrame;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.renderer.category.BarRenderer;
-import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.data.jdbc.JDBCCategoryDataset;
 
 /**
